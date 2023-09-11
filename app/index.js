0bdcc9e55ee08a343a10c86fb9e0355f63e928a9
williammunyakathuo <tjwellieyam@gmail.com>
2023-09-11 15:26:33 +0300

    native constants

--- app/index.js
+++ app/index.js
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native-web";
+import { useState } from "react";
+
+import { Stack, useRouter } from "expo-router";
+
+import {COLORS, icons, images, SIZES } from '../constants'
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
 const Home = () =>{
+    const router = useRouter()
+
     return(
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+            options={{
+                headerStyle:{backgroundColor: COLORS.lightWhite}
+            }}
+            />
+        </SafeAreaView>
     )
 }
 export default Home;
