af5b35d3aed3a76353e8b9fb4ae64447ae393873
williammunyakathuo <tjwellieyam@gmail.com>
2023-09-12 23:58:09 +0300

    popular jobs page ui/ux

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -8,54 +8,50 @@ import {
   ActivityIndicator,
 } from "react-native";
 
-import styles from "./popularjobs.style";
-import { COLORS, SIZES } from "../../../constants";
-import PopularJobCard from "../../common/cards/popular/PopularJobCard";
-import useFetch from "../../../hook/useFetch";
+import styles from "./popularjobs.style"
+import { COLORS, SIZES } from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import useFetch from '../../../hook/useFetch'
+
 
 const Popularjobs = () => {
-  const router = useRouter();
-  const { data, isLoading, error } = useFetch("search", {
-    query: "React developer",
-    num_pages: "1",
-  });
+  const router = useRouter()
 
-  const [selectedJob, setSelectedJob] = useState();
-
-  const handleCardPress = (item) => {
-    router.push(`/job-details/${item.job_id}`);
-    setSelectedJob(item.job_id);
-  };
+  const {data, isLoading, error} =useFetch(
+    'search', {
+      query: 'React developer',
+      num_pages : 1
+    }
+  )
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular jobs</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
+        <TouchableOpacity  >
+          <Text style={styles.headerBtn} >See all</Text>
         </TouchableOpacity>
       </View>
-
       <View style={styles.cardsContainer}>
-        {isLoading ? (
-          <ActivityIndicator size='large' color={COLORS.primary} />
-        ) : error ? (
-          <Text>Something went wrong</Text>
-        ) : (
-          <FlatList
-            data={data}
-            renderItem={({ item }) => (
-              <PopularJobCard
-                item={item}
-                selectedJob={selectedJob}
-                handleCardPress={handleCardPress}
-              />
-            )}
-            keyExtractor={(item) => item.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
-          />
+        {isLoading && (
+          <ActivityIndicator size="large" color={COLORS.primary} />
         )}
+        {error && (
+          <Text >
+            An error occoured here
+          </Text>
+        )}
+        {(!isLoading&& data)  && (<FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <PopularJobCard
+              item={item}
+            />
+          )}
+          keyExtractor={item => item?.job_id}
+          contentContainerStyle={{ columnGap: SIZES.medium }}
+          horizontal
+        />)}
       </View>
     </View>
   );
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,47 +1,49 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+
+
+
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null)
 
   const options = {
-    method: "GET",
+    method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-    headers: {
-      "X-RapidAPI-Key": '',
-      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
-    },
     params: { ...query },
-  };
-
-  const fetchData = async () => {
-    setIsLoading(true);
-
-    try {
-      const response = await axios.request(options);
-
-      setData(response.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setError(error);
-      console.log(error)
-    } finally {
-      setIsLoading(false);
+    headers: {
+      'X-RapidAPI-Key': '694ea0934fmshb221f27d654a927p1c23d5jsnd2b79fda8147',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     }
   };
 
-  useEffect(() => {
+  const fetchData = async () => {
+    setIsLoading(true)
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data)
+      setIsLoading(false)
+    } catch (error) {
+      setError(error)
+      alert('ther is an error')
+      setIsLoading(false)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(()=>{
     fetchData();
   }, []);
 
-  const refetch = () => {
+  const refetch = () =>{ 
     setIsLoading(true);
-    fetchData();
-  };
+    fetchData()
+  }
 
-  return { data, isLoading, error, refetch };
-};
+  return {data, isLoading, error, refetch}
+}
 
 export default useFetch;
